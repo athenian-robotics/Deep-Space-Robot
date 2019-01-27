f11e1a950d5c33a033ba3ba5897c8237fc907e45
Ezra Newman <ezra@manmans.com>
2019-01-27 15:37:42 -0800

    improve docs and add target'

--- src/main/java/frc/team852/command/ClimberMove.java
+++ src/main/java/frc/team852/command/ClimberMove.java
@@ -3,30 +3,40 @@ package frc.team852.command;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.team852.Robot;
 import frc.team852.RobotMap;
+import frc.team852.subsystem.ClimberSubsystem;
 
 public class ClimberMove extends Command {
     final double speed;
-
-    //Allow for speed to be passed through the motors
-    public ClimberMove(double speed) {
+    final double target;
+    final ClimberSubsystem climber = Robot.climberSubsystem;
+	
+	/**
+	 * @param speed the speed to move the climber at
+	 * @param target the target location, in encoder ticks, to move the motor to
+	 *               	//TODO convert to inches?
+	 *               may go slightly past the encoder position
+	 */
+    public ClimberMove(double speed, double target) {
         super();
         requires(Robot.climberSubsystem);
         this.speed = speed;
+        this.target = target;
     }
     //In case no speed is given, default to this
     public ClimberMove() {
-        this(1);
+        this(1, 70);
     }
 
-    //If interrupted, run end();
+    //Called when interrupted
     protected void interrupted() {
         end();
     }
 
     //Reset encoders, reset motors, put everything back to where it was.
     protected void end() {
-        RobotMap.climberMotor.resetEncoder();
-        RobotMap.climberSubsystem.stopMotors();
+        RobotMap.climberMotor.resetEncoder(); //TODO what do we want to do about this
+        climber.stopMotors();
+        //TODO return to hold command
     }
 
     //Never stop, change this later once we are no longer using buttons to start and stop
@@ -34,9 +44,11 @@ public class ClimberMove extends Command {
         return false;
     }
 
-    //Pass a speed through the motors
-    //TODO Implement encoders to know where the climber is relative to the ground or whatever you want it to be relative to.
+    //Pass a speed through the motors, stop when done
     protected void execute() {
-        RobotMap.climberMotor.set(speed);
+    	if (climber.getEncoderPos() < target)
+        	climber.setSpeed(speed);
+    	else
+    		end();
     }
 }
