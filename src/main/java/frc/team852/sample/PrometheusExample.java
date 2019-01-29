d23e8e5b68ccad0040a29e059fb805b8f4a4b62d
Adit Shah <aditshah00@gmail.com>
2019-01-29 08:34:47 -0800

    Update prometheus examples

--- src/main/java/frc/team852/sample/PrometheusExample.java
+++ src/main/java/frc/team852/sample/PrometheusExample.java
@@ -3,28 +3,26 @@ package frc.team852.sample;
 //see github if you have questions: https://github.com/prometheus/client_java
 
 //dont forget import
-import io.prometheus.client.*;
+
+import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
 
 public class PrometheusExample {
+
+    public static void main(String[] args) {
+        requests.inc(.5);
+        inprogressRequests.inc(2);
+        System.out.println(requests.get() + ", " + inprogressRequests.get());
+    }
 
     //Counter: Used to count things, can only increment ( inc() ) (usually by 1)
     static final Counter requests = Counter.build()
             .name("requests_total").help("Total requests.").register();
 
-    void requests(){
-        requests.inc();
-    }
 
     //Gauge: Used to track any value, can inc(), dec(), and set()
     static final Gauge inprogressRequests = Gauge.build()
             .name("inprogress_requests").help("Inprogress requests.").register();
-
-    void processRequest() {
-        inprogressRequests.inc();
-        inprogressRequests.dec();
-        inprogressRequests.set(5);
-    }
-
 
 
 }
