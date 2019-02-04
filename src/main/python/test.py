8f271e31afb73f507c47925d1b74b1e16e49a61b
Donovan Zhong <20dzhong@gmail.com>
2019-02-03 16:06:58 -0800

    implement vision-client-startup

---
+++ src/main/python/test.py
@@ -0,0 +1,18 @@
+import cv2
+
+cap = cv2.VideoCapture(1)
+
+while 1:
+    ret, frame = cap.read()
+    if ret:
+        # cv2.circle(frame, (335, 240), 7, (0, 255, 0), -1)
+        # cv2.circle(frame, (305, 240), 7, (0, 255, 0), -1)
+
+        cv2.circle(frame, (320, 240), 7, (0, 255, 0), -1)
+        cv2.imshow(str(frame.shape), frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
--- src/main/python/vision-client-startup.sh
+++ src/main/python/vision-client-startup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Tune exposure on camera
-v4l2-ctl -d /dev/video0 -c exposure_auto=1 -c exposure_absolute=20
+v4l2-ctl -d /dev/video1 -c exposure_auto=1 -c exposure_absolute=3 -c white_balance_temperature_auto=0 -c white_balance_temperature=5000
 
-python3 visionClient.py
+python3 test.py
