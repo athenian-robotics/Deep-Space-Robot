828e12a47e8480e19ebb958d5d14409e2ab48339
JTkeio <jacobtkeio@gmail.com>
2019-02-07 16:39:40 -0800

    streamline detectGaffeTape

--- src/main/python/cv_utils/processes/detectGaffeTape.py
+++ src/main/python/cv_utils/processes/detectGaffeTape.py
@@ -10,23 +10,29 @@ high = numpy.array([180, 190, 100])
 
 def detectGaffeTape(shared_frame: SharedFrame):
     frame = shared_frame.getFrame()
-
-    blurredframe = cv2.blur(frame, (5, 5))  # blur image
-    hsv = cv2.cvtColor(blurredframe, cv2.COLOR_BGR2HSV)  # change colorspace to HSV
-    colormask = cv2.inRange(hsv, low, high)  # find tape
-    contourmask, contours, hierarchy = cv2.findContours(colormask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)  # create a list of contours
+    # blur image
+    blurredframe = cv2.blur(frame, (5, 5))
+    # change colorspace to HSV
+    hsv = cv2.cvtColor(blurredframe, cv2.COLOR_BGR2HSV)
+    # find tape
+    colormask = cv2.inRange(hsv, low, high)
+    # create a list of contours
+    contourmask, contours, hierarchy = cv2.findContours(colormask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     if len(contours) > 0:
         ordered = sorted(contours, key=cv2.contourArea, reverse=True)  # arrange contours by area
-
-        contour = ordered[0]  # biggest contour
-        bottompt = sorted(contour, key=lambda a: a[0][1])[0][
-            0]  # find the point with the smallest y value in the contour
-        bottomobj = Point(bottompt[0], bottompt[1])  # create Point object ^^^
-        toppt = sorted(contour, key=lambda a: a[0][1])[-1][0]  # find the point with the largest y value in the contour
-        topobj = Point(toppt[0], toppt[1])  # create Point object ^^^
-        centroid = Point(int((bottompt[0] + toppt[0]) / 2),
-                         int((bottompt[1] + toppt[1]) / 2))  # find centroid as the average of ^^^
+        # biggest contour
+        contour = ordered[0]
+        # find the point with the smallest y value in the contour
+        bottompt = sorted(contour, key=lambda a: a[0][1])[0][0]
+        # create Point object ^^^
+        bottomobj = Point(bottompt[0], bottompt[1])
+        # find the point with the largest y value in the contour
+        toppt = sorted(contour, key=lambda a: a[0][1])[-1][0]
+        # create Point object ^^^
+        topobj = Point(toppt[0], toppt[1])
+        # find centroid as the average of ^^^
+        centroid = Point(int((bottompt[0] + toppt[0]) / 2), int((bottompt[1] + toppt[1]) / 2))
         if len(contour) > 5:
             # if there are enough points, find the angle of the contour (using fitEllipse)
             (x1, y1), (MA1, ma1), angle = cv2.fitEllipse(contour)
