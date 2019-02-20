66eb9f0c230c3c7b5273d9b32ffe3927431aa211
Ezra Newman <ezra@manmans.com>
2019-02-19 19:38:42 -0800

    ball actually works w toggle

--- src/main/java/frc/team852/command/Ball.java
+++ src/main/java/frc/team852/command/Ball.java
@@ -1,5 +1,6 @@
 package frc.team852.command;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.team852.OI;
 import frc.team852.Robot;
@@ -7,32 +8,25 @@ import frc.team852.subsystem.BallSubsystem;
 
 public class Ball extends Command {
 	private final BallSubsystem bs;
-	private long startTime;
-	private boolean done = false;
+	private static boolean ballIn = true;
 	
 	public Ball() {
 		requires(Robot.ballSubsystem);
 		bs = Robot.ballSubsystem;
 	}
+	
 	@Override
-	protected void execute(){
-		if(startTime + 100 > System.currentTimeMillis()) {
+	protected void initialize() {
+		ballIn = !ballIn;
+		if(ballIn){
+			bs.dropBall();
+		}else{
 			bs.grabBall();
-			done=true;
-			System.out.println("HERE-E2");
-			
 		}
 	}
 	
 	@Override
-	protected void initialize() {
-		startTime = System.currentTimeMillis();
-		bs.dropBall();
-		System.out.println("HERE-E1");
-	}
-	
-	@Override
 	protected boolean isFinished() {
-		return done;
+		return true;
 	}
 }
